13b63ff6b3ff913e823050403a2f394b27d47ce0
krystianklimczak <krystianklimczak1996r.com>
2023-10-22 17:40:18 +0200

    create component statistics  with logic, styles & proptypes

---
+++ src/components/statistics/Statistics.js
@@ -0,0 +1,47 @@
+// 'React.Component'
+import React, { Component } from 'react';
+
+// css modules
+import css from './Statistics.module.css';
+
+// proptypes
+import PropTypes from 'prop-types';
+
+export default class Statistics extends Component {
+
+    render() {
+        // destructuring assignment
+        const {good, neutral, bad, total, positiveFeedback} = this.props;
+
+        return (
+            <div className={css.statistics}>
+                <ul className={css.statistics__list}>
+                    <li className={css.statistics__item}>
+                        <p>Good: {good}</p>
+                    </li>
+                    <li className={css.statistics__item}>
+                        <p>Neutral: {neutral}</p>
+                    </li>
+                    <li className={css.statistics__item}>
+                        <p>Bad: {bad}</p>
+                    </li>
+                    <li className={css.statistics__item}>
+                        <p>Total: {total}</p>
+                    </li>
+                    <li className={css.statistics__item}>
+                        <p>Positive feedback: {positiveFeedback}</p>
+                    </li>
+                </ul>
+            </div>
+        )
+    }
+}
+
+// proptypes of statistics
+Statistics.propTypes = {
+    good: PropTypes.number.isRequired,
+    neutral: PropTypes.number.isRequired,
+    bad: PropTypes.number.isRequired,
+    total: PropTypes.number.isRequired,
+    positiveFeedback: PropTypes.string.isRequired,
+};
