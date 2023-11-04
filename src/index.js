00fb51f0189de1f79aaf2c451338dc7ae2d3b224
krystianklimczak <krystianklimczak1996r.com>
2023-11-04 13:11:34 +0100

    add context provider for app & format imports

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+
 import App from 'components/App';
+import { FeedbackProvider } from 'components/feedbackContext';
+
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <FeedbackProvider>
+      <App />
+    </FeedbackProvider>
   </React.StrictMode>
 );
