160293c99aa2fe667cce2ae1499bc1e5669e0e04
krystianklimczak <krystianklimczak1996r.com>
2023-11-04 13:09:54 +0100

    change FeedbackOptions class component to function component & format imports

--- src/components/feedbackOptions/FeedbackOptions.jsx
+++ src/components/feedbackOptions/FeedbackOptions.jsx
@@ -1,35 +1,31 @@
-// 'React.Component'
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 
-// css modules
+import { useFeedback } from 'components/feedbackContext';
+
 import css from './FeedbackOptions.module.css';
 
-// proptypes
-import PropTypes from 'prop-types';
+export default function FeedbackOptions() {
+  const [options] = useState(['good', 'neutral', 'bad']);
+  const { handleIncrement } = useFeedback();
 
-export default class FeedbackOptions extends Component {
-    render() {
-        const { options, onLeaveFeedback } = this.props;
-        
-        return (
-            <div className={css.feedback}>
-                <ul className={css.feedback__list}>
-                    {options.map(option => {
-                        return (
-                            <li key={option} className={css.feedback__item}>
-                                <button type='button' name={option} onClick={onLeaveFeedback} className={css.feedback__button}>
-                                    {option}
-                                </button>
-                            </li>
-                        )
-                    })}
-                </ul>
-            </div>
-        )
-    }
+  return (
+    <div className={css.feedback}>
+      <ul className={css.feedback__list}>
+        {options.map(option => {
+          return (
+            <li key={option} className={css.feedback__item}>
+              <button
+                type="button"
+                name={option}
+                onClick={handleIncrement}
+                className={css.feedback__button}
+              >
+                {option}
+              </button>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
 }
-
-FeedbackOptions.propTypes = {
-    options: PropTypes.arrayOf(PropTypes.string).isRequired,
-    onLeaveFeedback: PropTypes.func.isRequired,
-};
