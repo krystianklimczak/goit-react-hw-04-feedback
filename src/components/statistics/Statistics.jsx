a6426022026a37f8ea31ec152b07762962ef6806
krystianklimczak <krystianklimczak1996r.com>
2023-11-04 13:10:31 +0100

    change Statistics class component to function component & format imports

--- src/components/statistics/Statistics.jsx
+++ src/components/statistics/Statistics.jsx
@@ -1,46 +1,37 @@
-// 'React.Component'
-import React, { Component } from 'react';
+import { useFeedback } from 'components/feedbackContext';
 
-// css modules
 import css from './Statistics.module.css';
 
-// proptypes
-import PropTypes from 'prop-types';
+export default function Statistics() {
+  const { good, neutral, bad, total, positiveFeedback, handleReset } =
+    useFeedback();
 
-export default class Statistics extends Component {
-
-    render() {
-        const {good, neutral, bad, total, positiveFeedback, handleReset} = this.props;
-
-        return (
-            <div className={css.statistics}>
-                <ul className={css.statistics__list}>
-                    <li className={css.statistics__item}>
-                        <p>Good: {good}</p>
-                    </li>
-                    <li className={css.statistics__item}>
-                        <p>Neutral: {neutral}</p>
-                    </li>
-                    <li className={css.statistics__item}>
-                        <p>Bad: {bad}</p>
-                    </li>
-                    <li className={css.statistics__item}>
-                        <p>Total: {total}</p>
-                    </li>
-                    <li className={css.statistics__item}>
-                        <p>Positive feedback: {positiveFeedback}</p>
-                    </li>
-                <button type='button' className={css.statistics__btn} onClick={handleReset}>Reset</button>
-                </ul>
-            </div>
-        )
-    }
+  return (
+    <div className={css.statistics}>
+      <ul className={css.statistics__list}>
+        <li className={css.statistics__item}>
+          <p>Good: {good}</p>
+        </li>
+        <li className={css.statistics__item}>
+          <p>Neutral: {neutral}</p>
+        </li>
+        <li className={css.statistics__item}>
+          <p>Bad: {bad}</p>
+        </li>
+        <li className={css.statistics__item}>
+          <p>Total: {total}</p>
+        </li>
+        <li className={css.statistics__item}>
+          <p>Positive feedback: {positiveFeedback}</p>
+        </li>
+        <button
+          type="button"
+          className={css.statistics__btn}
+          onClick={handleReset}
+        >
+          Reset
+        </button>
+      </ul>
+    </div>
+  );
 }
-
-Statistics.propTypes = {
-    good: PropTypes.number.isRequired,
-    neutral: PropTypes.number.isRequired,
-    bad: PropTypes.number.isRequired,
-    total: PropTypes.number.isRequired,
-    positiveFeedback: PropTypes.string.isRequired,
-};
