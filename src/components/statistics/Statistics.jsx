e4c179ecdb74d9186b4f784f937b1d697966bb57
krystianklimczak <krystianklimczak1996r.com>
2023-11-05 09:15:32 +0100

    change title

--- src/components/statistics/Statistics.jsx
+++ src/components/statistics/Statistics.jsx
@@ -10,11 +10,11 @@ export default function Statistics() {
   return (
     <div className={css.statistics}>
       <ul className={css.statistics__list}>
-        <StatisticsItem title={'Good'} value={good} />
-        <StatisticsItem title={'Neutral'} value={neutral} />
-        <StatisticsItem title={'Bad'} value={bad} />
-        <StatisticsItem title={'Total'} value={total} />
-        <StatisticsItem title={'Positive Feedback'} value={positiveFeedback} />
+        <StatisticsItem title="Good" value={good} />
+        <StatisticsItem title="Neutral" value={neutral} />
+        <StatisticsItem title="Bad" value={bad} />
+        <StatisticsItem title="Total" value={total} />
+        <StatisticsItem title="Positive Feedback" value={positiveFeedback} />
         <button
           type="button"
           className={css.statistics__btn}
