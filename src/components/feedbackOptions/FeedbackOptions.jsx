b33bec575e58909323b0d877026610bb4c4fd203
krystianklimczak <krystianklimczak1996r.com>
2023-11-05 08:40:30 +0100

    seperate to smaller reusable components

--- src/components/feedbackOptions/FeedbackOptions.jsx
+++ src/components/feedbackOptions/FeedbackOptions.jsx
@@ -1,29 +1,16 @@
 import React, { useState } from 'react';
 
-import { useFeedback } from 'hooks/feedbackContext';
+import FeedbackOptionsItem from './FeedbackOptionsItem';
 
 import css from './FeedbackOptions.module.css';
 
 export default function FeedbackOptions() {
   const [options] = useState(['good', 'neutral', 'bad']);
-  const { handleIncrement } = useFeedback();
-
   return (
     <div className={css.feedback}>
       <ul className={css.feedback__list}>
         {options.map(option => {
-          return (
-            <li key={option} className={css.feedback__item}>
-              <button
-                type="button"
-                name={option}
-                onClick={handleIncrement}
-                className={css.feedback__button}
-              >
-                {option}
-              </button>
-            </li>
-          );
+          return <FeedbackOptionsItem option={option} key={option} />;
         })}
       </ul>
     </div>
---
+++ src/components/feedbackOptions/FeedbackOptionsItem.jsx
@@ -0,0 +1,20 @@
+import { useFeedback } from 'hooks/feedbackContext';
+import React from 'react';
+import css from './FeedbackOptions.module.css';
+
+export default function FeedbackOptionsItem(props) {
+  const { option } = props;
+  const { handleIncrement } = useFeedback();
+  return (
+    <li className={css.feedback__item}>
+      <button
+        type="button"
+        name={option}
+        onClick={handleIncrement}
+        className={css.feedback__button}
+      >
+        {option}
+      </button>
+    </li>
+  );
+}
